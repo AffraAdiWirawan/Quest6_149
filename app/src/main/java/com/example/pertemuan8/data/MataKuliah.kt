25004056b0ed9107b7282c85b812ce25b547320f
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-11-21 18:52:00 +0700

    Membuat objecr Matakuliah Dengan Listof

---
+++ app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan8.data
+
+object MataKuliah {
+    val options = listOf(
+        "Computer Vision",
+        "Pengembangan Aplikasi Web",
+        "Aplikasi Multimedia",
+        "Logika Teknik Pemrograman",
+        "Serious Game"
+    )
+}
