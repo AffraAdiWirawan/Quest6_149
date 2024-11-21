1d26580dd353c9af533a7ee883f68ca50deabf27
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-11-21 19:02:10 +0700

    Mengisi Wadaha data kelas dengan listof

--- app/src/main/java/com/example/pertemuan8/data/RuangKelas.kt
+++ app/src/main/java/com/example/pertemuan8/data/RuangKelas.kt
@@ -3,5 +3,10 @@ package com.example.pertemuan8.data
 import com.example.pertemuan8.R
 
 object RuangKelas {
-
+    val kelas = listOf(
+        "A",
+        "B",
+        "C",
+        "D",
+    )
 }
